3b27b53fc998d01293ac7d8217f5ae01f51b4349
Hsin-Pai Cheng <hc218@hl279-cmp-02.egr.duke.edu>
2019-04-01 14:47:11 -0400

    =.=

--- scripts/make_voc_tfrecords.sh
+++ scripts/make_voc_tfrecords.sh
@@ -6,14 +6,17 @@
 #   Editor      : VIM 
 #   File name   : make_voc_tfrecords.sh
 #   Author      : YunYang1994
+#   Edited by   : Dave Cheng
 #   Created date: 2019-01-21 11:40:10
 #   Description : 
 #
 #================================================================
-###
+
 python scripts/extract_voc.py --voc_path /home/hc218/data/VOC/train/ --dataset_info_path ./
-cat ./2007_train.txt ./2007_val.txt > voc_train.txt
+# use 2007_train, 2007_val, 2012_train, 2012_val for training
+cat ./2007_train.txt ./2007_val.txt ./2012_train.txt ./2012_val.txt > voc_train.txt
 python scripts/extract_voc.py --voc_path /home/hc218/data/VOC/test --dataset_info_path ./
+# use 2007_test for testing
 cat ./2007_test.txt > voc_test.txt
 python core/convert_tfrecord.py --dataset_txt ./voc_train.txt --tfrecord_path_prefix ./VOC_tfrecord/VOC_train
 python core/convert_tfrecord.py --dataset_txt ./voc_test.txt  --tfrecord_path_prefix ./VOC_tfrecord/VOC_test
